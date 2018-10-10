598cc22f54141c9e54a155d7e61d46cce57564dc
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-10-10 10:05:27 -0700

    Finished error handling exercise.

--- practice_05.py
+++ practice_05.py
@@ -15,7 +15,22 @@ def divide(x, y):
     except ZeroDivisionError as error:
         print(f"Cannot compute due to {error}")
 
+def ask():
+    response = None
+    squared = None
+    while not squared:
+      response = input("Input an integer: ")
+      try:
+        int(response)
+      except:
+        print("An error occured!  Please try again!")
+      else:
+        squared = int(response)**2
+        print(f"Thank you, your number squared is : {squared}")
+
+
 # Test Cases:
 
 # multiply(["a", "b", "c"])
 # divide(1, 0)
+ask()
